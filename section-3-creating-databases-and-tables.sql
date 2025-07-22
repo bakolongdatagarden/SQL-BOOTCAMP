6623600de5e73b05d4f0f74dd41825cb6fde1481
Nyambi Bakolong <bakolongdatagarden@proton.me>
2025-07-22 02:11:47 -0500

    More on tables: discuss data types (INT, VARCHAR)

--- section-3-creating-databases-and-tables.sql
+++ section-3-creating-databases-and-tables.sql
@@ -24,7 +24,7 @@ USE seed_library; # let's now use 'seed_library' (re-run previous command to ver
 
 
 -- =============================================
--- SECTION: Introducing Tables
+-- SECTION: Introducing Tables: Pt 1
 -- =============================================
 
 /* 
@@ -37,4 +37,23 @@ USE seed_library; # let's now use 'seed_library' (re-run previous command to ver
 ◘ But before we get more into tables ... we need to discuss types of data ... 
 */
 
+-- =============================================
+-- SECTION: Introducing Tables: Pt 2
+-- =============================================
 
+/*
+◘ When we define the structure of a table, we also specify what type of information is allowed
+◘ Types: Numeric? String? Date? 
+◘ Learn more about data types: https://dev.mysql.com/doc/refman/8.4/en/data-types.html
+
+INT
+◘ Integer is a whole number 
+	○ 12, 0, -9999
+    
+VARCHAR 
+◘ Variable length string
+	○ 'coffee!'
+    ○ '-9999' (this is a number, but it's in quotes so treated as a string)
+◘ We can specify a maximum number of characters allowed 
+	○ varchat(100)
+*/
