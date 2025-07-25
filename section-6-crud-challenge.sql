9bdc37dc7ff5d99dd85ad6ea8ba0b59af1438e8d
Nyambi Bakolong <bakolongdatagarden@proton.me>
2025-07-25 13:54:37 -0500

    CRUD Challenge: UPDATE: We update certain data points.

--- section-6-crud-challenge.sql
+++ section-6-crud-challenge.sql
@@ -21,9 +21,9 @@ SCENARIO : Spring Cleaning (The Annual Closet Inventory)
     ◘ 4. Add a new shirt: purple polo shirt, size M, last worn 50 days ago
     ◘ 5. SELECT all shirts: but only print out article and color
     ◘ 6. SELECT all medium shirts: print out everything but shirt_id
-    ◘ UPDATE all polo shirts: change their size to L
-    ◘ UPDATE the shirt last worn 15 days ago: change last_worn to 0
-    ◘ UPDATE all white shirts: change size to 'XS' and color to 'off white'
+    ◘ 7. UPDATE all polo shirts: change their size to L
+    ◘ 8. UPDATE the shirt last worn 15 days ago: change last_worn to 0
+    ◘ 9. UPDATE all white shirts: change size to 'XS' and color to 'off white'
     ◘ DELETE all old shirts: last worn 200 days ago
     ◘ DELETE all tank tops
     ◘ DELETE all shirts
@@ -31,7 +31,7 @@ SCENARIO : Spring Cleaning (The Annual Closet Inventory)
 */
 
 -- =================================================
--- CREATING
+-- CREATE
 -- =================================================
 
 -- 1. CREATE a new database to keep track of the shirts 
@@ -71,7 +71,7 @@ INSERT INTO shirts (article, color, shirt_size, last_worn)
 VALUES('polo shirt', 'purple', 'M', 50);
 
 -- =================================================
--- READING
+-- READ
 -- =================================================
 SELECT * FROM shirts;
 
@@ -82,4 +82,41 @@ SELECT article, color FROM shirts;
 -- 6. SELECT all medium shirts: print out everything but shirt_id
 SELECT article, color, shirt_size, last_worn FROM shirts
 WHERE shirt_size='M';
+
+-- =================================================
+-- UPDATE
+-- =================================================
+-- Toggle Safe Updates
+SET SQL_SAFE_UPDATES = 0; # temporarily shut off safe update
+SET SQL_SAFE_UPDATES = 1; # turn safe update back on 
+
+SELECT * FROM shirts;
+
+-- 7. UPDATE all polo shirts: change their size to 'L'
+UPDATE shirts SET shirt_size='L'
+WHERE article='polo shirt';
+
+-- 8. UPDATE the shirt last worn 15 days ago: change last_worn to 0
+-- a. inspect first
+SELECT * FROM shirts
+WHERE last_worn=15;
+
+-- b. now update
+UPDATE shirts SET last_worn=0
+WHERE last_worn=15;
+
+SELECT * FROM shirts;
+
+-- 9. UPDATE all white shirts: change size to 'XS' and color to 'off white'
+-- a. inspect first
+SELECT * FROM shirts 
+WHERE color='white';
+
+-- b. update
+UPDATE shirts SET shirt_size='XS', color='off white'
+WHERE color='white';
+
+-- =================================================
+-- DELETE
+-- =================================================
 
