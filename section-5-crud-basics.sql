a27a2394c9a6a27e9471d1501ce669a5b2953f50
Nyambi Bakolong <bakolongdatagarden@proton.me>
2025-07-24 22:34:35 -0500

    We explore the 'C' in 'CRUD': Create. We delete the old cats table and CREATE a new one, insert 7 cats to begin our dataset.

--- section-5-crud-basics.sql
+++ section-5-crud-basics.sql
@@ -1,4 +1,4 @@
-# SECTION 5: CRUD BASICS
+-- SECTION 5: CRUD BASICS
 /*
 This section is all about CRUD
 - The 4 basic actions we want to be able to do to our data are:
@@ -7,3 +7,41 @@ This section is all about CRUD
 	◘ Update 
 	◘ Delete
 */
+
+-- =================================================
+-- Getting Our New Dataset
+-- ===================================================
+
+SHOW TABLES;
+
+-- let's drop the original cats table 
+DROP TABLE cats;
+
+-- C = CREATE
+
+-- create a new 'cats' table
+CREATE TABLE cats (
+	cat_id INT AUTO_INCREMENT,
+    name VARCHAR(100),
+    breed VARCHAR(100),
+    age INT,
+    PRIMARY KEY (cat_id)
+);
+
+DESC cats;
+
+-- Insert some data
+INSERT INTO cats (name, breed, age)
+VALUES 
+	('Ringo', 'Tabby', 4),
+    ('Cindy','Maine Coon', 10),
+    ('Dumbledore','Main Coon', 11),
+    ('Egg','Persian', 4),
+    ('Misty','Tabby', 13),
+    ('George Michael','Ragdoll', 9),
+	('Jackson','Sphynx', 7);
+    
+-- Inspect our data
+SELECT * FROM cats;
+    
+    
