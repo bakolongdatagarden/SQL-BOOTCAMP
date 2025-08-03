c1b477b680bcb060200d90cfd188058d51615b28
Nyambi Bakolong <bakolongdatagarden@proton.me>
2025-08-02 22:39:16 -0500

    Move this section 7 notes to colt-bootcamp folder.

--- colt-bootcamp/sec-7-string-functions.sql
+++ colt-bootcamp/sec-7-string-functions.sql
@@ -249,4 +249,129 @@ FROM books;
 
 -- Here's your village name
 SELECT 
-	REPLACE(
+CONCAT_WS('l', 'Nyambi', 'ten');
+
+-- ###################################
+-- PRACTICE 
+-- ###################################
+
+/*
+QUESTION 1
+	Write a query that displays:
+	• each book's title
+    • concatenated with the text "by" 
+    • and the author's full name (full name + space + last name)
+    
+    Ex.
+    • "The Namesake by Jhumpa Lahiri"
+*/
+
+USE book_shop;
+SELECT * FROM books;
+
+SELECT 
+    CONCAT(title,
+            ' by ',
+            author_fname,
+            ' ',
+            author_lname) AS 'Title & Author'
+FROM
+    books;
+
+/*
+QUESTION 2
+	Using the books table, write a query that shows:
+	• only the first 10 characters of each book title
+    • but add "..." at the end to indicate it's been shortened
+*/
+
+SELECT 
+    CONCAT(SUBSTR(title, 1, 10), '...') AS 'Shortened Titles'
+FROM
+    books;
+    
+/*
+QUESTION 3
+	Write a query that creates:
+	• author initials in the format "First Initial. Last Initial." (ex "J.L." or N.G.")
+	• and also shows the full author name	
+
+	Your result should have two columns:
+
+		• One showing initials like "J.L.", "N.G.", "D.E.", etc.
+		• One showing the full name like "Jhumpa Lahiri", "Neil Gaiman", "Dave Eggers", etc.
+*/
+
+SELECT * FROM books;
+SELECT 
+    CONCAT(SUBSTR(author_fname, 1, 1),
+            '.',
+            SUBSTR(author_lname, 1, 1),
+            '.') AS 'Initials',
+    CONCAT_WS(' ', author_fname, author_lname) AS 'Full Name'
+FROM
+    books;
+
+/*
+QUESTION 4
+	Write a query that displays:
+	• book titles where all spaces have been replaced with underscores (_),
+	• and give this column the alias "web_friendly_title"
+*/
+
+SELECT 
+    REPLACE(title, ' ', '_') AS 'web_friendly_title'
+FROM
+    books;
+    
+/*
+QUESTION 5
+	Write a query that combines multiple string functions to create a formatted book summary. 
+    For each book, display:
+		• "Book: [first 15 characters of title]...by[Author Intials] ([released_year])"
+    
+		Your result should look something like:
+			• "Book: The Namesake... by J.L. (2003)"
+			• "Book: Norse Mythology... by N.G. (2016)"
+			• "Book: American Gods... by N.G. (2001)"
+*/
+
+SELECT 
+    CONCAT('Book: ',
+            SUBSTR(title, 1, 15),
+            '...',
+            ' by ',
+            SUBSTR(author_fname, 1, 1),
+            '.',
+            SUBSTR(author_lname , 1, 1),
+            '.',
+            ' (',
+            released_year,
+            ')') AS 'Book Slug'
+FROM
+    books;
+    
+/*
+QUESTION 6
+	Write a query that creates a "book code" by combining:
+	• The first 3 characters of the author's last name (in uppercase)
+	• a dash (-)
+	• the last 2 digits of the release year
+	• a dash (-)
+	• the first letter of the title (in lowercase)    
+    
+	Your result should look something like:
+
+	• "LAH-03-t" (for "The Namesake" by Jhumpa Lahiri, 2003)
+	• "GAI-16-n" (for "Norse Mythology" by Neil Gaiman, 2016)
+*/
+
+SELECT 
+	CONCAT(
+		UPPER(SUBSTRING(author_lname, 1, 3)),
+        '-', 
+        SUBSTRING(released_year, -2, 2),
+        '-',
+        LOWER(SUBSTRING(title, 1, 1))) AS 'Book Code'
+FROM books;
+
